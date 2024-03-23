bb0a268d306f70fdcc5bbaf0838048e2e06dd42e
frederic <fgibou@ucsb.edu>
2024-03-23 16:27:04 -0700

    karla adds feature 1, 2, 10

--- main-karla-vincent.cpp
+++ main-karla-vincent.cpp
@@ -3,3 +3,6 @@ cout << "this is karla"
 cout << "this is Frederic gibou"
 cout << "this is laurent"
 cout << "karla changes main.cpp"
+karla adds feature 1 better than vincent's feature 1
+karla adds feature 2 worse than vincent's version
+karla adds feature 10
